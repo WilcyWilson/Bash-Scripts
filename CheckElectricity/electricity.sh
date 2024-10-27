e75aa92631739a7d86cef723d1a7c5645b1f36ad
wilcywilson <wilsonshresthaapps@gmail.com>
2024-10-27 17:25:07 +0545

    ping method changed

--- CheckElectricity/electricity.sh
+++ CheckElectricity/electricity.sh
@@ -8,7 +8,11 @@ URL="http://192.168.1.102:8080/message?token=A1hUZtU1CMJv3bk" #Locally hosted Go
 read internet_was_up_before </home/orangepi/scripts/internet_was_up_before.txt
 read was_internet_down </home/orangepi/scripts/was_internet_down.txt
 read count_fails </home/orangepi/scripts/count_fails.txt
-if ping -c 1 192.168.1.2 &>/dev/null || ping -c 1 192.168.1.3 &>/dev/null; then #Pings two routers for redundancy
+ping -c 20 192.168.1.2 # Try once.
+rc=$?
+ping -c 20 192.168.1.3 # Try once.
+mc=$?
+if [ "$rc" -eq 0 ] || [ "$mc" -eq 0 ]; then #Pings two routers for redundancy
     echo "success: $MESSAGE"
     if $was_internet_down; then
         curl -s -S --data '{"message": "'"${MESSAGE}"'", "title": "'"${ELECTRICITYUP}"'", "priority":'"${PRIORITY}"', "extras": {"client::display": {"contentType": "text/markdown"}}}' -H 'Content-Type: application/json' "$URL"
